71b402fe51cab19928bf089bc004a43776bab703
glgstyle <gleguenno@gmail.com>
2023-11-17 15:53:21 +0100

    Configuration of Circle/CI

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,6 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 # DEBUG = False
+# commentaires
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 ALLOWED_HOSTS = ['*']
