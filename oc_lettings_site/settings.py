95ca742b4a722ef0bb9294316e272ce0c5b97119
glgstyle <gleguenno@gmail.com>
2023-11-19 05:25:58 +0100

    Changing settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -23,8 +23,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://lettings.onrender.com/']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://lettings.onrender.com/']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
